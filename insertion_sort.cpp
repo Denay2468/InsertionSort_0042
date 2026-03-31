c97a98b9577f046cb7538f7eef791428452f42e4
Denay2468 <denayaung13@gmail.com>
2026-03-31 14:32:59 +0630

    Initial commit: add base project structure

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -4,68 +4,10 @@ using namespace std;
 int arr[20];
 int n;
 
-void input() {
-    while (true) {
-        cout << "Enter the number of elements in the array: ";
-        cin >> n;
-
-        if (n <= 20) {
-            break;
-        } else {
-            cout << "\nArray cannot have more than 20 elements.\n";
-        }
-    }
-
-    cout << endl;
-    cout << "==============================================\n";
-    cout << "============= Enter Array Elements ===========\n";
-    cout << "==============================================\n";
-
-    for (int i = 0; i < n; i++) {
-        cout << "Element " << (i + 1) << ": ";
-        cin >> arr[i]; // Fixed: was arr[1]
-    }
-}
-
-void insertionSort() {
-    int temp;
-    int j;
-
-    for (int i = 0; i < n; i++) {
-        temp = arr[i];
-        j = i - 1;
-
-        while (j >= 0 && arr[j] > temp) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-
-        arr[j + 1] = temp;
-
-        cout << "\nStep " << i << ": ";
-        for (int k = 0; k < n; k++) {
-            cout << arr[k] << " "; // Fixed: was cout << ""
-        }
-    }
-}
-
-void display() {
-    cout << endl;
-    cout << "\n================\n";
-    cout << "Element Array\n";
-    cout << "================\n";
-
-    for (int j = 0; j < n; j++) {
-        cout << arr[j] << endl;
-    }
-
-    cout << endl;
-}
+void input() {}
+void insertionSort() {}
+void display() {}
 
 int main() {
-    input();
-    insertionSort();
-    display();
-    system("pause");
     return 0;
 }
