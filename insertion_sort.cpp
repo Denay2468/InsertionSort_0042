531ecbb445879a40712f2b32d9d43dd992da742b
Denay2468 <denayaung13@gmail.com>
2026-03-31 14:35:47 +0630

    Add input() function with validation and array input

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -5,8 +5,24 @@ int arr[20];
 int n;
 
 void input() {
-    cout << "Enter the number of elements in the array: ";
-    cin >> n;
+    while (true) {
+        cout << "Enter the number of elements in the array: ";
+        cin >> n;
+        if (n <= 20) {
+            break;
+        } else {
+            cout << "\nArray cannot have more than 20 elements.\n";
+        }
+    }
+    cout << endl;
+    cout << "==============================================\n";
+    cout << "============= Enter Array Elements ===========\n";
+    cout << "==============================================\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Element " << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+
 }
 void insertionSort() {}
 void display() {}
