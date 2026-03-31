45f01bc9dc7a5bb90de827b672ff277a18873f3d
Denay2468 <denayaung13@gmail.com>
2026-03-31 14:37:27 +0630

    Add display() function to show sorted array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -41,7 +41,16 @@ void insertionSort() {
         }
     }
 }
-void display() {}
+void display() {
+    cout << endl;
+    cout << "\n================\n";
+    cout << "Element Array\n";
+    cout << "================\n";
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
 
 int main() {
     return 0;
